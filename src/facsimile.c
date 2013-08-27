b3fd17829eab2390d9e47ce5ce4fb279633cdefd
Calvin Beck <hobbes@ualberta.ca>
2013-08-27 14:44:26 -0600

    Added a couple more helper function. Mainly one to write the unit declarations for the hub.

--- src/facsimile.c
+++ src/facsimile.c
@@ -44,7 +44,7 @@ static void out_name(FILE *out_file, int out_pin)
 }
 
 
-static void out_chan(FILE *out_file, int out_pin)
+static void out_channel(FILE *out_file, int out_pin)
 {
     fprintf(out_file, "out%d_chan", out_pin);
 }
@@ -76,6 +76,77 @@ static void write_channel_inputs(FILE *out_file,
 
 	    in_name(out_file, current.in_pin);
 	}
+    }
+}
+
+
+static int int_in_array(int number, int *array, size_t array_length)
+{
+    for (size_t count = 0; count < array_length; ++count) {
+	int current = array[count];
+
+	if (current == number) {
+	    return 1;
+	}
+    }
+
+    return 0;
+}
+
+static void write_output_decs(FILE *out_file, MuxPipe *pipes, size_t total_pipes)
+{
+    int outputs[total_pipes];
+    size_t output_count = 0;
+
+    int inputs[total_pipes];
+    size_t input_count = 0;
+
+    /* Output declarations */
+    for (size_t index = 0; index < total_pipes; ++index) {
+	MuxPipe pipe = pipes[index];
+
+	/* Check if we have already recorded the output */
+	if (int_in_array(pipe.out_pin, outputs, output_count)) {
+	    continue;
+	}
+
+	/* Write the output declaration */
+	write_indents(out_file, 2);
+	fprintf(out_file, "output ");
+	out_name(out_file, pipe.out_pin);
+	fprintf(out_file, " port %d = false;\n", pipe.out_pin);
+
+	/* Write the channel declaration for the output */
+	write_indents(out_file, 2);
+	fprintf(out_file, "int ");
+	out_channel(out_file, pipe.out_pin);
+	fprintf(out_file, " = %d;\n\n", pipe.channel);
+
+	/* Record in the outputs array */
+	outputs[output_count] = pipe.out_pin;
+	++output_count;
+    }
+
+    fprintf(out_file, "\n");
+
+    /* Input declarations */
+    for (size_t index = 0; index < total_pipes; ++index) {
+	MuxPipe pipe = pipes[index];
+
+	/* Check if we have already recorded the output */
+	if (int_in_array(pipe.in_pin, inputs, input_count)) {
+	    continue;
+	}
+
+	/* Write the output declaration */
+	write_indents(out_file, 2);
+	fprintf(out_file, "input ");
+	in_name(out_file, pipe.in_pin);
+	fprintf(out_file, " port %d;\n", pipe.in_pin);
+
+	/* Record in the inputs array */
+	inputs[input_count] = pipe.in_pin;
+	++input_count;
     }
 }
 
